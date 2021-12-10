a062e3e7b12d03c48049336f7c0f26f3f0cbb12e
pariyaard <1024342@mborijnland.nl>
2021-12-10 14:12:28 +0100

    new layout, new adapter

---
+++ app/src/main/java/com/example/testfirebase/ItemAdapter.java
@@ -0,0 +1,39 @@
+package com.example.testfirebase;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+public class ItemAdapter extends FirebaseRecyclerAdapter<
+        Woord, ItemAdapter.ItemViewholder> {
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public ItemAdapter(@NonNull FirebaseRecyclerOptions<Woord> options) {
+        super(options);
+    }
+    @Override
+    protected void onBindViewHolder(@NonNull ItemAdapter.ItemViewholder holder, int position, @NonNull Woord model) {
+    }
+    @NonNull
+    @Override
+    public ItemAdapter.ItemViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+    public class ItemViewholder extends RecyclerView.ViewHolder {
+        public ItemViewholder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
+
+
