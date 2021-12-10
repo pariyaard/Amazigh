c129d81725fd8ef351e69e01765dae51ec8eb0f1
pariyaard <1024342@mborijnland.nl>
2021-12-10 14:03:45 +0100

    made new activity, make woord,

--- app/src/main/java/com/example/testfirebase/MainActivity2.java
+++ app/src/main/java/com/example/testfirebase/MainActivity2.java
@@ -1,28 +1,32 @@
 package com.example.testfirebase;
 
-import android.os.Bundle;
-
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity2 {
+public class MainActivity2 extends AppCompatActivity {
+
     private RecyclerView recyclerView;
+    //WoordAdapter adapter;
     DatabaseReference algemeen, woorden;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
 
 
-            // reference naar de data
-            woorden = FirebaseDatabase.getInstance().getReference("woorden").getRef();
+        // reference naar de data
+        woorden = FirebaseDatabase.getInstance().getReference("woorden").getRef();
 // klaarzetten van de recyclerview
-    recyclerView = recyclerView.findViewById();
-    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-}
+        recyclerView = findViewById(R.id.recycler1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 }
